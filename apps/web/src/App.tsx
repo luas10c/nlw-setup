90b3bc5b89f096884a78bd10f5c47e0d38dd56ad
Luciano Alves <luas10c@gmail.com>
2023-01-18 01:05:35 -0300

    feat(web): include header and summary-table component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from './components/header'
+import { SummaryTable } from './components/summary-table'
+
 const App = () => {
   return (
-    <div className="bg-blue-500">
-      <h4>App</h4>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
