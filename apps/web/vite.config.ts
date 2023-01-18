187f320e948b4edb6db3c94c39a5699d5552caf7
Luciano Alves <luas10c@gmail.com>
2023-01-17 23:59:23 -0300

    feat(web): add dependencies @svgr/rollup and svg types

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from '@svgr/rollup'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), svgr()]
 })
