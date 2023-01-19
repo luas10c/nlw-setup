5c0973e68040137db9370075b0f7d3ea4468c132
Luciano Alves <luas10c@gmail.com>
2023-01-19 07:47:27 -0300

    feat(mobile): add onpress on habbit-day

--- apps/mobile/src/components/habit-day/habit-day.tsx
+++ apps/mobile/src/components/habit-day/habit-day.tsx
@@ -2,11 +2,18 @@ import { TouchableOpacity } from 'react-native'
 
 import { DAY_SIZE } from '#/constants'
 
-export const HabitDay = () => {
+interface Props {
+  onPress?(): void
+}
+
+export const HabitDay = (props: Props) => {
+  const { onPress } = props
+
   return (
     <TouchableOpacity
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
+      onPress={onPress}
       activeOpacity={0.7}
     />
   )
