db126a6ec7bb2775735e22c4559de4919af1ef73
Luciano Alves <luas10c@gmail.com>
2023-01-19 05:54:21 -0300

    feat(web): add progress-bar component

---
+++ apps/web/src/components/progress-bar/progress-bar.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  value?: number
+}
+
+export const ProgressBar = (props: Props) => {
+  const { value = 20 } = props
+
+  return (
+    <div className="relative h-4 rounded-xl bg-zinc-700 w-full mt-4 overflow-hidden">
+      <div
+        className="absolute top-0 left-0 bg-violet-500 h-4"
+        aria-valuemin={0}
+        aria-valuenow={value}
+        aria-valuemax={100}
+        style={{ width: `${value}%` }}
+      ></div>
+    </div>
+  )
+}
