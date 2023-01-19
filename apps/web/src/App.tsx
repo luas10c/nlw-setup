7ec726a81aef993c93dad67b8e5b859ba1bfb7a7
Luciano Alves <luas10c@gmail.com>
2023-01-19 05:58:01 -0300

    feat(web): add overflow-hidden on container

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,7 +3,7 @@ import { SummaryTable } from './components/summary-table'
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="w-screen h-screen flex items-center justify-center overflow-hidden">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
         <SummaryTable />
