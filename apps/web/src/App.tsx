f758f5a4239aa9c9932b02b64d13fea72711e0bf
Luciano Alves <luas10c@gmail.com>
2023-01-20 05:52:59 -0300

    chore(web): add dependêncies lock file

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,13 @@
-import { Header } from './components/header'
-import { SummaryTable } from './components/summary-table'
+import { Toaster } from 'react-hot-toast'
+
+import { Home } from './pages/home'
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center overflow-hidden">
-      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <Header />
-        <SummaryTable />
-      </div>
-    </div>
+    <>
+      <Home />
+      <Toaster />
+    </>
   )
 }
 
