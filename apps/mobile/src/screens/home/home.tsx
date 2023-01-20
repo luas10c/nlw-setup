87a172409d25fa44554819c97ee0b5a603903bec
Luciano Alves <luas10c@gmail.com>
2023-01-20 07:10:48 -0300

    feat(mobile): add fetch summary

--- apps/mobile/src/screens/home/home.tsx
+++ apps/mobile/src/screens/home/home.tsx
@@ -1,5 +1,7 @@
-import { View, ScrollView, Text } from 'react-native'
+import { useState, useEffect } from 'react'
+import { View, ScrollView, Text, ToastAndroid, Alert, Platform } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import dayjs from 'dayjs'
 
 import { generateDatesFromYearBegining } from '#/utils/generate-dates-from-year-begining'
 
@@ -7,14 +9,51 @@ import { DAY_SIZE } from '#/constants'
 
 import { Header } from '#/components/header'
 import { HabitDay } from '#/components/habit-day'
+import { api } from '#/lib/axios'
+import { Loading } from '#/components/loading'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const datesFromYearsStart = generateDatesFromYearBegining()
 const minimumSummaryDatesSizes = 18 * 5
 const ammountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearsStart.length
 
+interface Summary {
+  id: string
+  date: Date
+  amount: number
+  completed: number
+}
+
 export const Home = () => {
   const { navigate } = useNavigation()
+  const [loading, setLoading] = useState(true)
+  const [summary, setSummary] = useState<Summary[]>([])
+
+  useEffect(() => {
+    const fetchSummary = async () => {
+      try {
+        setLoading(true)
+        const { data } = await api.get<{ summary: Summary[] }>('/summary')
+        const { summary } = data
+        setSummary(summary)
+      } catch (error) {
+        if (error instanceof Error) {
+          if (Platform.OS === 'android') {
+            ToastAndroid.show(error.message, 100)
+          } else {
+            Alert.alert(error.message)
+          }
+        }
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchSummary()
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
     <View className="bg-background flex-1 px-8 pt-16">
@@ -39,9 +78,14 @@ export const Home = () => {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearsStart.map((date) => {
+            const dayInSummary = summary.find((day) =>
+              dayjs(date).isSame(day.date, 'day')
+            )
+
             return (
               <HabitDay
                 key={date.toString()}
+                data={dayInSummary}
                 onPress={() => navigate('ViewHabit', { date: date.toISOString() })}
               />
             )
