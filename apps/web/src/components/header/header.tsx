071f08f78ea7486572cc6ae3ef007c823a78421a
Luciano Alves <luas10c@gmail.com>
2023-01-21 04:05:09 -0300

    feat(web): add focus-ring button create habit on header component

--- apps/web/src/components/header/header.tsx
+++ apps/web/src/components/header/header.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
 
         <button
           type="button"
-          className="border border-violet-500 rounded-lg px-6 py-4 flex items-center gap-3 font-semibold uppercase hover:border-violet-300 transition-colors"
+          className="border-2 border-transparent border-violet-300 hover:border-violet-500 rounded-lg px-6 py-4 flex items-center gap-3 font-semibold uppercase focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background transition-colors"
           onClick={() => setOpenedCreateHabit((oldState) => !oldState)}
         >
           <Plus size={20} className="text-violet-500" />
