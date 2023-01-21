d0a3a0e6bc9708e21ae08added67626c63e527e7
Luciano Alves <luas10c@gmail.com>
2023-01-21 04:04:17 -0300

    feat(web): add focus-ring and disabled prop on checkbox component

--- apps/web/src/components/checkbox/checkbox.tsx
+++ apps/web/src/components/checkbox/checkbox.tsx
@@ -3,22 +3,29 @@ import clsx from 'clsx'
 
 interface Props {
   checked?: boolean
-  onChange?(value: boolean): void
+  disabled?: boolean
+  onChange: (value: boolean) => void
   description?: string
 }
 
 export const Checkbox = (props: Props) => {
-  const { checked, onChange = () => {}, description } = props
+  const { checked, disabled, onChange = () => {}, description } = props
 
   return (
     <button
       type="button"
-      onClick={() => onChange(!checked)}
-      className="flex gap-3 mt-6 items-center cursor-pointer select-none"
+      onClick={() => {
+        if (disabled) {
+          return
+        }
+
+        onChange(!checked)
+      }}
+      className="flex gap-3 mt-2 items-center cursor-pointer select-none group outline-none"
     >
       <div
         className={clsx(
-          'bg-zinc-900 border-2 border-zinc-800 w-8 h-8 rounded-lg flex items-center justify-center',
+          'bg-zinc-900 border-2 border-zinc-800 w-8 h-8 rounded-lg group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background flex items-center justify-center transition-colors',
           {
             'bg-green-600 border-green-700': checked
           }
