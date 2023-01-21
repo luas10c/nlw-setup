bc60995b4eec118be366cf11dbd1f5fac0c0cccc
Luciano Alves <luas10c@gmail.com>
2023-01-21 04:03:23 -0300

    feat(web): render habit-day only length higher 0

--- apps/web/src/components/summary-table/summary-table.tsx
+++ apps/web/src/components/summary-table/summary-table.tsx
@@ -48,19 +48,22 @@ export const SummaryTable = () => {
         })}
       </div>
 
-      <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => {
-          const dayInSummary = summary.find((day) => dayjs(date).isSame(day.date, 'day'))
+      <div className="grid grid-rows-7 grid-flow-col gap-3 py-1">
+        {summary.length > 0 &&
+          summaryDates.map((date) => {
+            const dayInSummary = summary.find((day) =>
+              dayjs(date).isSame(day.date, 'day')
+            )
 
-          return (
-            <HabitDay
-              key={date.toString()}
-              date={date}
-              amount={dayInSummary?.amount}
-              completed={dayInSummary?.completed}
-            />
-          )
-        })}
+            return (
+              <HabitDay
+                key={date.toString()}
+                date={date}
+                amount={dayInSummary?.amount}
+                defaultCompleted={dayInSummary?.completed}
+              />
+            )
+          })}
         {minimumSummaryDatesSize > 0 &&
           Array.from({ length: minimumSummaryDatesSize })
             .fill(new Date())
